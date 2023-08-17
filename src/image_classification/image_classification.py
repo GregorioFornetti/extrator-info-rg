501b8aadbded4ca42016faab1e3fd4d26fc9b345
GregorioFornetti <63006135+GregorioFornetti@users.noreply.github.com>
2023-08-17 01:27:19 -0300

    finalizando...

--- src/image_classification/image_classification.py
+++ src/image_classification/image_classification.py
@@ -3,9 +3,14 @@ import cv2
 
 
 img_size = (150, 112)
-model = load('image_classification/lr_model.joblib')
+model = load('image_classification/svm_model.joblib')
 
 
 def get_rg_probability(img):
     resized_img = cv2.resize(img, img_size).flatten()
-    return model.predict_proba([resized_img])[0][1]
+    pred = model.predict([resized_img])[0]
+    if pred == -1:
+        pred = 'Não é RG'
+    else:
+        pred = 'É RG'
+    return pred
